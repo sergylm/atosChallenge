a6a9ad3316809244139e36779aa78a29d99e4983
sergylopez21 <sergy_lopez_21@hotmail.com>
2021-03-07 13:08:22 +0100

    intento de frontend

--- src/localizacion/algoritmo.py
+++ src/localizacion/algoritmo.py
@@ -8,27 +8,17 @@ def sumar (x,y):
 
 def getSolarData(latitud, longitud):
     URL = ("https://power.larc.nasa.gov/cgi-bin/v1/DataAccess.py?&request=execute&tempAverage=CLIMATOLOGY&identifier=SinglePoint&parameters=SI_EF_TILTED_SURFACE&userCommunity=SB&lon=%s&lat=%s&outputList=CSV&user=DOCUMENTATION" %(longitud,latitud))
-    print(URL)
     r = requests.get(URL)
     data = r.json()
     csv = data['outputs']['csv'] #url del csv
     parameters = data['features'][0]['properties']['parameter'] #valores medios mensual y anual por parametro
     parametersInfo = data['parameterInformation'] #nombre completo y unidades de parametros
-    """for parameter in parameters:
-        print(parameters[parameter])
-    for parameter in parametersInfo:
-        print(parametersInfo[parameter]['longname'])
-        print(parametersInfo[parameter]['units'])"""
     for para in parameters:
         parameters[para].insert(0,parametersInfo[para]['longname'] + "("+ parametersInfo[para]['units'] + ')')
     context={
         "para": parameters,
         "paraInfo": parametersInfo
     }
-    """print(parameters)
-    for para in parameters:
-        for dato in parameters[para]:
-            print(dato)"""
     aux = []
     for para in parameters:
         aux.append(parameters[para])
@@ -55,5 +45,5 @@ def geoCode():
     formatted_address = data['results'][0]['formatted_address'] 
 
     print("Latitud: " + latitude + " Longitud: " + longitude + "\n Dirección: " + formatted_address)
-"""
+    """
     pass
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -32,6 +32,22 @@ var searchControl = new GeoSearchControl({
 
 map.addControl(searchControl);
 
+//collapsible
+var coll = document.getElementsByClassName('collapsible');
+var i;
+
+for(i = 0;i< coll.length; i++){
+    coll[i].addEventListener('click', function(){
+        this.classList.toggle('activate');
+        var content = this.nextElementSibling;
+        if(content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    });
+}
+
 //csrf token
 $.ajaxSetup({
     beforeSend: function(xhr, settings) {
