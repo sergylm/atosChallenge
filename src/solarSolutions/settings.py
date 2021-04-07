bab5a9ab8bc56eedf46a281647d8d418c47e1826
sergylopez21 <sergy_lopez_21@hotmail.com>
2021-04-07 23:26:29 +0200

    heroku

--- src/solarSolutions/settings.py
+++ src/solarSolutions/settings.py
@@ -132,4 +132,4 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
